e21e890171e40fad8971f40f69eb7322b7b488a7
Christie <cholvie@gmail.com>
2019-04-17 22:41:27 -0400

    Added tester method to Context.js as comment and  fixed errors

--- src/context.js
+++ src/context.js
@@ -9,14 +9,30 @@ class ProductProvider extends Component {
     state ={
         products:storeProducts,
         detailProduct:detailProduct
-    }
+    };
     handleDetail = () =>{
         console.log('hello from detail');
-    }
+    };
 
     addToCart = () =>{
         console.log('hello from add to cart');
-    }
+    };
+  
+// TESTER METHOD: un-comment the FOLLOWING tester code to verify functionality in the console.
+//   tester = () => {
+//     console.log('State products :', this.state.products[0].inCart);
+//     console.log('Data products :', storeProducts[0].inCart);
+
+//     const tempProducts = [...this.state.products];
+//     tempProducts[0].inCart = true
+// this.setState(() => {
+//     return {products:tempProducts}
+// },() => {
+//     console.log("State products :", this.state.products[0].inCart);
+//     console.log("Data products :", storeProducts[0].inCart);
+// })
+
+// }  
     
   render() {
     return (
